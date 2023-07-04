b9b0dec2d702090ec5f2a2b739c1a38dd6736449
tamilselvan09 <tamilselvan4982@gmail.com>
2023-07-04 12:31:10 +0530

    lang

---
+++ lang/src/lang/AL.java
@@ -0,0 +1,19 @@
+package lang;
+
+import java.util.ArrayList;
+
+class Emp {
+	
+}
+public class AL{
+	
+
+	public static void main(String[] args)  {
+		AL a1=new AL();
+		Emp e1 = new Emp();
+		String ename = new String("Rahul");
+		System.out.println(ename);  //Rahul
+		System.out.println(a1);     //@
+		System.out.println(e1);
+	}
+}
