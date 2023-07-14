acafad42d02308b9adf820c606f5eb6961aaeab3
tamilselvan09 <tamilselvan4982@gmail.com>
2023-07-14 10:45:16 +0530

    sort

--- cfexamples/src/ArrayListFive/DataOperaction.java
+++ cfexamples/src/ArrayListFive/DataOperaction.java
@@ -2,6 +2,7 @@ package ArrayListFive;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Iterator;
 
 public class DataOperaction {
@@ -21,6 +22,19 @@ public class DataOperaction {
 		enames.add("AB");
 		
 		
+		System.out.println(enames); //[Vignesh, Mano, Naveen, Vasu, Praveen, Sumanth, AB]
+		
+		//sort mean AtoZ , 1to9 in order
+		Collections.sort(enames);
+		System.out.println(enames);  //[AB, Mano, Naveen, Praveen, Sumanth, Vasu, Vignesh]
+		
+		//reverse order ZtoA , 9t01
+		Collections.sort(enames,Collections.reverseOrder()); 
+		System.out.println(enames);   //[Vignesh, Vasu, Sumanth, Praveen, Naveen, Mano, AB]
+		
+		//Every time is showing diffrent 
+		System.out.println(enames);
+		Collections.shuffle(enames);
 		System.out.println(enames);
 		
 		
