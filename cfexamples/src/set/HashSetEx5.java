24e27377d3176f59b58c247cf82d5ce6aa2f3752
tamilselvan09 <tamilselvan4982@gmail.com>
2023-07-17 12:38:55 +0530

    dupli

--- cfexamples/src/set/HashSetEx5.java
+++ cfexamples/src/set/HashSetEx5.java
@@ -6,28 +6,33 @@ import java.util.Iterator;
 public class HashSetEx5 {
 
 	public static void main(String[] args) {
-		HashSet<Integer> hs=new HashSet<Integer>(10,0.80f);
 		
-		hs.add(10);
-		hs.add(20);
-	//	hs.add("Rahul");  //We mention integer but we right string so its not 
-		hs.add(30);
-		hs.add(40);
-			
-		System.out.println(hs);  //[20,40,10,30]
-		
-		//Intexing consecpt is not there so we cant use normal loop 
-		//Iterrater only for each and iterrator method
-		
-		//iterator
-		
-		 Iterator i= hs.iterator();
-		 while(i.hasNext()) {
-			 System.out.println(i.next());  //20,40,10,30
-		 }
-		
-		
-		
+	HashSet	hs=new HashSet();
+	   //duplicates are not allowed
+	  // so only one 10 will print
+	
+	hs.add(10);
+	hs.add(10);
+	hs.add(10);
+	hs.add(10);
+	hs.add(10);
+	hs.add(10);
+	hs.add(10);
+	hs.add(10);
+	hs.add(10);
+	hs.add(10);
+	
+	hs.add("Tamil");
+	hs.add(null);
+	hs.add(true);
+	hs.add(11);
+	
+	
+	System.out.println(hs);  // [null, 10, 11, Tamil, true]
+	
+		for(Object obj:hs) {
+			System.out.println(obj);
+		}
 	}
 
 }
