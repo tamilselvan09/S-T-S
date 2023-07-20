0b6f94f1bf79dd9d7fa6d51593d078e60cda7ad5
tamilselvan09 <tamilselvan4982@gmail.com>
2023-07-20 12:05:53 +0530

    queue

--- cfexamples/src/queue/PriorityQueueone5.java
+++ cfexamples/src/queue/PriorityQueueone5.java
@@ -8,9 +8,9 @@ public class PriorityQueueone5 {
 	public static void main(String[] args) {
 		PriorityQueue pq=new PriorityQueue();
 		
-		System.out.println(pq);
-		//System.out.println(pq.element());
-		System.out.println(pq.peek());
+		System.out.println(pq);  //[]
+		System.out.println(pq.element());      //There is no read so element create one exception 
+		System.out.println(pq.peek());      //Peek return null
 		
 	}
 
