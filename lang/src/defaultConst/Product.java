d11f1e0ff716fb883724146d2fb0e3f5cfeb6f6a
tamilselvan09 <tamilselvan4982@gmail.com>
2023-07-24 23:52:32 +0530

    defultcont

---
+++ lang/src/defaultConst/Product.java
@@ -0,0 +1,25 @@
+package defaultConst;
+
+public class Product {
+	
+	int prodId;
+	static String prodCat;
+	//default constructer
+	
+	Product(){
+		super();
+	}
+	
+}
+class Demo{
+      public static void main(String[] args) {
+		Product p1 = new Product();
+		Product p2 = new Product();
+		
+		System.out.println(p1.prodId);  //0
+		System.out.println(p2.prodId);  //0
+		System.out.println(p2.prodCat);  //null
+
+	}
+
+}
