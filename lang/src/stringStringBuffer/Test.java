646432efb3390514effc6da4af93b08484e73825
tamilselvan09 <tamilselvan4982@gmail.com>
2023-07-24 11:03:27 +0530

    string

---
+++ lang/src/stringStringBuffer/Test.java
@@ -0,0 +1,15 @@
+package stringStringBuffer;
+
+
+//StringBuffer
+
+public class Test {
+
+	public static void main(String[] args) {
+	StringBuffer	sb  = new StringBuffer("Tamil");
+	sb.append("Selvan");
+	
+	System.out.println(sb);  //TamilSelvan
+	}
+
+}
---
+++ lang/src/stringStringBuffer/stringTest.java
@@ -0,0 +1,13 @@
+package stringStringBuffer;
+
+public class stringTest {
+
+	public static void main(String[] args) {
+
+		String s = new String ("Tamil");
+		s.concat("Selvan");
+		System.out.println(s);
+
+	}
+
+}
