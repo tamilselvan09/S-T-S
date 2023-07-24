35e6d4790dba77fc752bed718a6cd2dcc98d14c8
tamilselvan09 <tamilselvan4982@gmail.com>
2023-07-24 12:18:43 +0530

    stringobj

---
+++ lang/src/stringObject/Test.java
@@ -0,0 +1,19 @@
+package stringObject;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String ename1 = new String ("Tamil");
+		String ename2 = new String ("Selvan");
+
+		 ename2.concat("Gandhi"); 
+		
+		System.out.println(ename1+ename2);
+		System.out.println(ename1);
+		System.out.println(ename2);
+		
+		
+	}
+
+}
+	
---
+++ lang/src/stringObject/Testone.java
@@ -0,0 +1,23 @@
+package stringObject;
+
+public class Testone {
+
+	public static void main(String[] args) {
+	
+		
+		Testone obj1 = new Testone();
+		Testone obj2 = new Testone();
+		
+		int a = 10;
+		int b = 20;
+		
+		// boolean flag = a.equals(b);
+		 boolean flag = obj2.equals(b);
+		
+		System.out.println(a==b);
+		 //System.out.println(a-b+a);
+		
+
+	}
+
+}
